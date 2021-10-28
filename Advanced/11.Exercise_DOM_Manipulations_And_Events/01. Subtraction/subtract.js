37397c1aec906d05beb4c25bc9c952dce4ea1bbc
Sspirdonov <Svetomir.Spirdonov@gmail.com>
2021-10-28 18:16:48 +0300

    Refresh data on 28.10.2021

--- Advanced/11.Exercise_DOM_Manipulations_And_Events/01. Subtraction/subtract.js
+++
@@ -1,9 +0,0 @@
-function subtract() {
-    //let result = document.getElementById('result');
-    let num1 = Number( document.getElementById('firstNumber').value);
-    let num2 = Number( document.getElementById('secondNumber').value);
-    //result.innerHTML = num1 - num2 ;
-    let res = num1 - num2 ;
-    document.getElementById('result').textContent = res ;
-
-}
